54c8200122929f615cc9287a910008704e2945b6
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-16 23:49:44 -0400

    Up start bot.py

--- bot.py
+++ bot.py
@@ -98,9 +98,19 @@ async def helpstr(message: types.Message):
     keyboard_markup.row(btns)
     FIRST = message.from_user.first_name
     MSG = f'''
-Hello {FIRST}, Im {BOT_NAME}
-U can find my Boss  <a href="tg://user?id={OWNER}">HERE</a>
-Cmds /chk /info /gen /bin'''
+👋 ¡Hola {FIRST}! Soy {BOT_NAME}, tu asistente bancario virtual.
+
+📚 Aquí tienes una lista de comandos que puedes usar:
+    /chk <code> - Verifica una tarjeta de crédito.
+    /info - Muestra información sobre el usuario.
+    /gen <bin> - Genera detalles de una tarjeta de crédito.
+    /bin <bin> - Obtiene información sobre un BIN.
+
+🔗 También puedes encontrarme en:
+    <a href="tg://user?id={OWNER}">Mi Creador</a>
+
+🌐 Además, puedes revisar mi código fuente en GitHub:
+'''
     await message.answer(MSG, reply_markup=keyboard_markup,
                         disable_web_page_preview=True)
 
